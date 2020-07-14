d0d771d314283d69bcbb4527108061ceaee12a7a
Garrett Johnson <gjohnson49@liberty.edu>
2020-07-14 13:04:29 -0400

    A practice ground for common programming skills.

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -1,5 +1,5 @@
 ﻿//The purpose of this program is to practice the common things you'll use for a coding assessment. 
-// 1. string manipulation, 2. Array traversal, 3. Class instantiation, 4. recursion. 
+// 1. String manipulation, 2. Array traversal, 3. Class instantiation, 4. Recursion, 5. Control flow
 using System;
 
 namespace CodingPractice
