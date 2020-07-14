4e83ad4d944f0fb28fe234a1026d1693c5f1576d
Garrett Johnson <gjohnson49@liberty.edu>
2020-07-14 13:02:39 -0400

    Add project files.

---
+++ CodingPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿//The purpose of this program is to practice the common things you'll use for a coding assessment. 
+// 1. string manipulation, 2. Array traversal, 3. Class instantiation, 4. recursion. 
+using System;
+
+namespace CodingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
